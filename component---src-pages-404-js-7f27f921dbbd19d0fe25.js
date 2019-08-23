8e473bf9d96c61974e9d621c2b4608f225fb911d
”liaoyeguo <yeguoliao@gmail.com>
2019-08-23 16:30:31 +0800

    Updates

---
+++ component---src-pages-404-js-7f27f921dbbd19d0fe25.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{181:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),r=t(69),u=t.n(r);n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(u.a,null,l.a.createElement("title",null,"I goofed it.")),l.a.createElement("div",null,"404——没有找到这个页面"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7f27f921dbbd19d0fe25.js.map
